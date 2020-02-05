2853edaef3a8aee14e82601bfbba8a9bf05ad58f
macotok <monochromer@r8.ucom.ne.jp>
2020-02-05 23:55:09 +0900

    create dataをpostするまで

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res.status(200).send('Hello from the server side!');
 // });
@@ -20,6 +22,23 @@ app.get('/api/v1/tours', (req, res) => {
     data: {
       tours,
     }
+  })
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = {
+    id: newId,
+    ...req.body,
+  };
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      }
+    })
   })
 });
 
